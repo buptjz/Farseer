b13a8268d475bfc3cc70373a53f7242f76f66516
dreamStar <huan.le.song@163.com>
2015-01-20 00:29:26 +0800

    增加try catch

--- entrance/views.py
+++ entrance/views.py
@@ -29,30 +29,33 @@ reply wechat users' messages.
 request:POST query
 """        
 def reply_msg(request):
-    query_xml = ET.fromstring(request.body)
-    
-    wechat_user = query_xml.find('FromUserName').text   
-    dev_user = query_xml.find('ToUserName').text
-    msg_type = query_xml.find('MsgType').text    
-    msg = query_xml.find('Content').text
-    msg_ID = query_xml.find('MsgId')
-    
-    #msg = msg.decode("utf-8")
-    ret_msg = []
-    if msg.find(u'向兆威') != -1:
-        ret_msg.append("向大脸！")
-    elif msg.find(u"王继哲") != -1:
-        ret_msg.append("小短腿！")
-    else:
-        ret_msg.append("猫王大好人！")
-    ret_dic = dict()
-    ret_dic['ToUserName'] = wechat_user
-    ret_dic['FromUserName'] = dev_user
-    ret_dic['CreateTime'] = int(time.time())
-    ret_dic['MsgType'] = 'text'
-    ret_dic['Content'] = "".join(ret_msg)
-    
-    return render_to_response('reply_msg.xml', ret_dic, mimetype="application/xml")
+    try:
+        query_xml = ET.fromstring(request.body)
+        
+        wechat_user = query_xml.find('FromUserName').text   
+        dev_user = query_xml.find('ToUserName').text
+        msg_type = query_xml.find('MsgType').text    
+        msg = query_xml.find('Content').text
+        msg_ID = query_xml.find('MsgId')
+        
+        #msg = msg.decode("utf-8")
+        ret_msg = []
+        if msg.find(u'向兆威') != -1:
+            ret_msg.append("向大脸！")
+        elif msg.find(u"王继哲") != -1:
+            ret_msg.append("小短腿！")
+        else:
+            ret_msg.append("猫王大好人！")
+        ret_dic = dict()
+        ret_dic['ToUserName'] = wechat_user
+        ret_dic['FromUserName'] = dev_user
+        ret_dic['CreateTime'] = int(time.time())
+        ret_dic['MsgType'] = 'text'
+        ret_dic['Content'] = "".join(ret_msg)
+        
+        return render_to_response('reply_msg.xml', ret_dic, mimetype="application/xml")
+    except:
+        return HttpResponse("parse error!")
         
 def index(request):
     try:
