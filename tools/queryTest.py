9a691dc34237b58f8fe099e6851a859145568148
dreamStar <huan.le.song@163.com>
2015-01-22 16:38:40 +0800

    new tool file

--- tools/queryTest.py
+++ tools/queryTest.py
@@ -6,6 +6,29 @@ Created on Thu Jan 22 00:13:18 2015
 """
 import urllib,urllib2
 
+def text_from_wechat(data_dict):
+    data = ["<xml>\n"]
+    data.append("<ToUserName><![CDATA[")
+    data.append(data_dict['ToUserName'])
+    data.append("]]></ToUserName>\n")
+    data.append("<FromUserName><![CDATA[")
+    data.append(data_dict['FromUserName'])
+    data.append("]]></FromUserName>\n")
+    data.append("<CreateTime>")
+    data.append(data_dict['CreateTime'])
+    data.append("</CreateTime>\n")
+    data.append("<MsgType><![CDATA[")
+    data.append("text")
+    data.append("]]></MsgType>\n")
+    data.append("<Content><![CDATA[")
+    data.append(data_dict['Content'])
+    data.append("]]></Content>\n")
+    data.append("<MsgId>")
+    data.append(data_dict['MsgId'])
+    data.append("</MsgId>\n")
+    data.append("</xml>")
+    return "".join(data)
+"""
 def post_data(url,app_para_dct):
     content = ""
     if app_para_dct:
@@ -13,15 +36,26 @@ def post_data(url,app_para_dct):
         f = urllib2.urlopen(url, para_data)
         content = f.read()
     return content
+"""
+def post_data(url,app_para_dict):
+    cookies = urllib2.HTTPCookieProcessor()
+    opener = urllib2.build_opener(cookies)
+ 
+    request = urllib2.Request(
+        url = url,
+        data = app_para_dict)
+ 
+    return opener.open(request).read()
     
     
 if __name__ == "__main__":
     post_dict = dict()
     post_dict['FromUserName'] = '123'
     post_dict['ToUserName'] = '123'
-    post_dict['MsgType'] = '123'
+    post_dict['MsgType'] = 'text'
     post_dict['Content'] = '123'
     post_dict['MsgId'] = '123'
-    post_dict['text'] = '123'
+    post_dict['CreateTime'] = '123'
+    print text_from_wechat(post_dict)
     
-    print post_data("http://rumordetector.sinaapp.com/entrance",post_dict)
+    print post_data("http://rumordetector.sinaapp.com/entrance",text_from_wechat(post_dict))
