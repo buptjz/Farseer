de26a1948dda15d9d502e798cf24b9f6327a610e
dreamStar <huan.le.song@163.com>
2015-01-22 09:58:07 +0800

    添加模拟query的工具

---
+++ tools/queryTest.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 22 00:13:18 2015
+
+@author: catking
+"""
+import urllib,urllib2
+
+def post_data(url,app_para_dct):
+    content = ""
+    if app_para_dct:
+        para_data = urllib.urlencode(app_para_dct)
+        f = urllib2.urlopen(url, para_data)
+        content = f.read()
+    return content
+    
+    
+if __name__ == "__main__":
+    post_dict = dict()
+    post_dict['FromUserName'] = '123'
+    post_dict['ToUserName'] = '123'
+    post_dict['MsgType'] = '123'
+    post_dict['Content'] = '123'
+    post_dict['MsgId'] = '123'
+    post_dict['text'] = '123'
+    
+    print post_data("http://rumordetector.sinaapp.com/entrance",post_dict)
